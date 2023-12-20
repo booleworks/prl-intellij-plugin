7b0a7c8eb4bfb46523776d6d7cb5361a22a92479
Steffen Hildebrandt <steffen.hildebrandt@booleworks.com>
2023-12-20 17:26:07 +0100

    initial commit

---
+++ src/main/gen/com/boolerules/prl/plugin/psi/FeatureRestriction.java
@@ -0,0 +1,19 @@
+// This is a generated file. Not intended for manual editing.
+package com.boolerules.prl.plugin.psi;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+
+public interface FeatureRestriction extends PsiElement {
+
+  @Nullable
+  Num getNum();
+
+  @Nullable
+  PosNegNumber getPosNegNumber();
+
+  @Nullable
+  QuotedString getQuotedString();
+
+}
